aa5a97f8d00e95979d261bac56fdd9af66d1254f
Anton Bortnikov <toha1100@gmail.com>
2022-07-03 20:35:34 +0500

    feat: setup vercel deploy

--- remix.config.js
+++ remix.config.js
@@ -3,5 +3,7 @@
  */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
+  serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
